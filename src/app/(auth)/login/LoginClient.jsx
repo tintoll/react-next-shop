3c3e047423b4a644177a4a359ef91accebf4dc70
tintoll <belselios@gmail.com>
2023-12-14 20:11:29 +0900

    로그인 페이지 css 완성

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -4,18 +4,38 @@ import React, { useState } from "react";
 import LogoPath from "@/assets/colorful.svg";
 import Image from "next/image";
 
+import styles from "./Auth.module.scss";
+import { useRouter } from "next/navigation";
+
 const LoginClient = () => {
-  const [value, setValue] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [isLoading, setisLoading] = useState(false);
+  const [isAutoLogin, setisAutoLogin] = useState(false);
+
+  const router = useRouter();
+
+  const redirectUser = () => {
+    router.push("/");
+  };
+
+  const loginUser = (e) => {
+    e.preventDetault();
+    setisLoading(true);
+  };
+
+  const signInWithGoogle = () => {};
+
   return (
-    <section>
-      <div>
-        <h1>
+    <section className={styles.page}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>
           <Image src={LogoPath} alt="logo" property />
         </h1>
-        <form>
+        <form onSubmit={loginUser} className={styles.form}>
           {/* Input */}
-          <div>{/* 자동 로그인, 비밀번호 수정*/}</div>
-          <div>
+          <div className={styles.group}>{/* 자동 로그인, 비밀번호 수정*/}</div>
+          <div className={styles.buttonGroup}>
             {/* Button */}
             <div>{/* Button */}</div>
           </div>
