ed6bc64ea874601d62e2ffdd30ab3b58886be269
Lee Jae hwan <blue@nethru.co.kr>
2023-12-27 15:46:56 +0900

    toast provider 추가

--- src/components/toastProvider/ToastProvider.jsx
+++ src/components/toastProvider/ToastProvider.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 
 const ToastProvider = () => {
-  return <div>ToastProvider</div>;
+  return <ToastContainer autoClose={2000} />;
 };
 
 export default ToastProvider;
