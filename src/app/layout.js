d048327ba3109dc7669720942d6c5cfdf1c9239e
tintoll <belselios@gmail.com>
2023-12-29 08:38:33 +0900

    Header 만들기

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ToastProvider from "@/components/toastProvider/ToastProvider";
 import Footer from "@/layouts/footer/Footer";
+import Header from "@/layouts/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <ToastProvider />
         {children}
         <Footer />
--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import InnerHeader from "../innerHeader/InnerHeader";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "@/firebase/firebase";
+import styles from "./Header.module.scss";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
-  return <div>Header</div>;
+  const pathname = usePathname();
+
+  const isLoggedIn = true;
+  const [displayName, setDisplayName] = useState("");
+
+  // 사용자 이름을 가져오기 위한 부분
+  useEffect(() => {
+    // 구글에서 계정의 상태가 변경될때 호출되는 함수가 있다.
+    // 구글로그인하면 displayName이 이있지만 email/password 방식으 없다.
+    // 그래서 아래 로직에서 만들어준다.
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if (user) {
+        if (user.displayName === null) {
+          const u1 = user.email.substring(0, user.email.indexOf("@"));
+          const uName = u1.charAt(0).toUpperCase() + u1.slice(1);
+          setDisplayName(uName);
+        } else {
+          setDisplayName(user.displayName);
+        }
+
+        //  유저 정보를 리덕스 스토어에 저장하기
+      } else {
+        setDisplayName("");
+        // 유저 정보를 리덕스 스토어에서 지우기
+      }
+    });
+  }, []);
+
+  const logoutUser = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success("로그아웃 되었습니다.");
+        router.push("/");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
+  // 헤더가 필요없는 부분은 제외
+  if (
+    pathname === "/login" ||
+    pathname === "/register" ||
+    pathname === "/reset"
+  ) {
+    return null;
+  }
+  return (
+    <header>
+      <div className={styles.loginBar}>
+        <ul className={styles.list}>
+          {!isLoggedIn ? (
+            <li className={styles.item}>
+              <Link href={"/login"}>로그인</Link>
+            </li>
+          ) : (
+            <>
+              <li className={styles.item}>
+                <Link href={"/admin/dashboard"}>관리자</Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/order-history"}>주문 목록</Link>
+              </li>
+              <li className={styles.item}>
+                <Link href={"/"} onClick={logoutUser}>
+                  로그아웃
+                </Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/"}>제휴 마케팅</Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/"}>쿠팡 플레이</Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/"}>고객센터</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+      {pathname.startsWith("/admin") ? null : <InnerHeader />}
+    </header>
+  );
 };
 
 export default Header;
