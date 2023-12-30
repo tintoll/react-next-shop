3ba4b17a3b47493579f8317eae3e11a56b2efdf3
tintoll <belselios@gmail.com>
2023-12-30 12:12:31 +0900

    product filter 로직 작성

--- src/components/product/productFilter/ProductFilter.jsx
+++ src/components/product/productFilter/ProductFilter.jsx
@@ -1,6 +1,51 @@
-import React from "react";
+import {
+  selectMaxPrice,
+  selectMinPrice,
+  selectProducts,
+} from "@/redux/slice/productSlice";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const ProductFilter = () => {
+  const [category, setCategory] = useState("All");
+  const [brand, setBrand] = useState("All");
+  const [price, setPrice] = useState(10000);
+
+  const dispatch = useDispatch();
+
+  const products = useSelector(selectProducts);
+  const minPrice = useSelector(selectMinPrice);
+  const maxPrice = useSelector(selectMaxPrice);
+
+  const allCatetoies = [
+    "All",
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filterCategories = (category) => {
+    setCategory(category);
+    dispatch(FILTER_BY_CATEGORY({ products, category }));
+  };
+
+  const allBrands = [
+    "All",
+    ...new Set(products.map((product) => product.brand)),
+  ];
+
+  useEffect(() => {
+    dispatch(FILTER_BY_BRAND({ products, brand }));
+  }, [brand, dispatch, products]);
+
+  useEffect(() => {
+    dispatch(FILTER_BY_PRICE({ products, price }));
+  }, [price, dispatch, products]);
+
+  const clearFilters = () => {
+    setBrand("All");
+    setCategory("All");
+    setPrice(maxPrice);
+  };
+
   return <div>ProductFilter</div>;
 };
 
