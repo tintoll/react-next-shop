35c8b8b3b66b1129e83204c32318e642f50d9a27
tintoll <belselios@gmail.com>
2024-01-02 05:26:16 +0900

    상품 수정

--- src/app/admin/edit-product/[id]/page.jsx
+++ src/app/admin/edit-product/[id]/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import EditProductClient from "./EditProductClient";
 
 const EditProduct = () => {
-  return <div>EditProduct</div>;
+  return <EditProductClient />;
 };
 
 export default EditProduct;
